9dfe5f4f9b9cc202f0dad84456d236312bd618ed
Louis Dachet <louisd@bam.tech>
2022-03-17 21:56:58 +0100

    :fire: (app) clean app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:screen_limit/auth.dart';
-import 'package:screen_limit/profile.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -22,15 +19,7 @@ class ScreenLimitApp extends StatelessWidget {
       title: 'Screen Limit App',
       theme: ThemeData(primarySwatch: Colors.purple),
       home: Scaffold(
-        body: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return const ProfilePage();
-            }
-            return const AuthGate();
-          },
-        ),
+        body: Container(),
       ),
     );
   }
